0c86b8da5d3c17e6b7c66ba30f4bbbffd4c22238
61030069 <61030069@go.buu.ac.th>
2020-12-04 17:43:57 +0700

    first commit

---
+++ app/app/AccountModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountModel extends Model
+{
+    protected $table="Account";
+    protected $fillable=[
+        'Acc_No','Type_No','ACC_Name','ACC_Surname','Address','SubDistrict',
+        'District','Province','ZipCode','DateOp','Balance'
+    ];
+}
